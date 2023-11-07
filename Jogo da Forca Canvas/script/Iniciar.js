01c965bfdf44dcaa4cdfc86d58f8cbb92b40dd28
KalCastro <127865406+KalCastro@users.noreply.github.com>
2023-11-07 19:04:28 -0300

    Add files via upload

---
+++ Jogo da Forca Canvas/script/Iniciar.js
@@ -0,0 +1,7 @@
+const iniciarButton = document.getElementById("inicioButton");
+
+iniciarButton.addEventListener("click", () => {
+    // Redirecionar para a página principal do jogo
+    window.location.href = "forca.html";
+});
+

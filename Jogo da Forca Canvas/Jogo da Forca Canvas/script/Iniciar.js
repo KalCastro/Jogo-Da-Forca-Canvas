2ee6e0df4e23acb4d8b633c9aa62f6021a45df70
KalCastro <127865406+KalCastro@users.noreply.github.com>
2023-11-07 19:04:00 -0300

    Delete Jogo da Forca Canvas/Jogo da Forca Canvas directory

--- Jogo da Forca Canvas/Jogo da Forca Canvas/script/Iniciar.js
+++
@@ -1,7 +0,0 @@
-const iniciarButton = document.getElementById("inicioButton");
-
-iniciarButton.addEventListener("click", () => {
-    // Redirecionar para a página principal do jogo
-    window.location.href = "forca.html";
-});
-
